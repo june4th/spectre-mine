11daeb59dafcc8a09e128c76e9a318213e948dcd
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-08-04 00:48:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,25 +5,27 @@ apt-get install proot -y
 apt-get install git -y
 cd ~
 git clone https://github.com/MFDGaming/ubuntu-in-termux.git
+if [ ! -f ~/.bashrc ]; then
+  echo "~/ubuntu-in-termux/.startubuntu.sh" > ~/.bashrc
+else
+  if ! grep -Fxq "~/ubuntu-in-termux/.startubuntu.sh" ~/.bashrc; then
+    echo "~/ubuntu-in-termux/.startubuntu.sh" >> ~/.bashrc
+  fi
+fi
 cd ubuntu-in-termux
 chmod +x ubuntu.sh
 ./ubuntu.sh -y
 ./startubuntu.sh
 apt-get update -y
 apt-get upgrade -y
-apt-get install git wget build-essential cmake clang libssl-dev libudns-dev libc++-dev lld libsodium-dev
-git clone https://github.com/Tritonn204/tnn-miner.git
-cd tnn-miner
-mkdir build
-cd build
-cmake ..
-make
-echo "./TNN_Miner --spectre --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qz053xfu5fknjyzlf2rsf2uz7fyvjjhgrurlv2lclqmufu4x4uzfq6gxyjsdd --dev-fee 2 --worker-name JOY3" > start.sh
+git clone https://github.com/june4th/spectre-mine.git
+cd spectre-mine
+echo "./tnn-miner --spectre --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qz053xfu5fknjyzlf2rsf2uz7fyvjjhgrurlv2lclqmufu4x4uzfq6gxyjsdd --threads 8 --dev-fee 2 --worker-name JOY3" > start.sh
 chmod +x start.sh
 if [ ! -f ~/.bashrc ]; then
-  echo "cd ~/tnn-miner/build/ && sh start.sh" > ~/.bashrc
+  echo "cd ~/spectre-mine/ && sh start.sh" > ~/.bashrc
 else
-  if ! grep -Fxq "cd ~/tnn-miner/build/ && sh start.sh" ~/.bashrc; then
-    echo "cd ~/tnn-miner/build/ && sh start.sh" >> ~/.bashrc
+  if ! grep -Fxq "cd ~/spectre-mine/ && sh start.sh" ~/.bashrc; then
+    echo "cd ~/spectre-mine/ && sh start.sh" >> ~/.bashrc
   fi
 fi
