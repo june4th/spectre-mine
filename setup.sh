9a0a1f371b573ae291d69d2a012ec387ba6a2ace
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-08-04 02:31:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ yes | pkg install git wget proot
 cd ~
 git clone https://github.com/MFDGaming/ubuntu-in-termux.git
 if [ ! -f ~/.bashrc ]; then
-  echo "cd ~/ubuntu-in-termux/ && sh startubuntu.sh" > ~/.bashrc
+  echo "cd ~/ubuntu-in-termux/ && ./startubuntu.sh" > ~/.bashrc
 else
-  if ! grep -Fxq "cd ~/ubuntu-in-termux/ && sh startubuntu.sh" ~/.bashrc; then
-    echo "cd ~/ubuntu-in-termux/ && sh startubuntu.sh" >> ~/.bashrc
+  if ! grep -Fxq "cd ~/ubuntu-in-termux/ && ./startubuntu.sh" ~/.bashrc; then
+    echo "cd ~/ubuntu-in-termux/ && ./startubuntu.sh" >> ~/.bashrc
   fi
 fi
 cd ubuntu-in-termux
@@ -22,9 +22,9 @@ cd spectre-mine
 echo "./tnn-miner --spectre --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qz0qlynwptd4lgzh4vy9pz8g3vkw8v97jkpp7dudenzqzt7qayw32rjzlkgfs --threads 8 --dev-fee 2 --worker-name JOY3" > start.sh
 chmod +x start.sh
 if [ ! -f ~/.bashrc ]; then
-  echo "cd ~/spectre-mine/ && sh start.sh" > ~/.bashrc
+  echo "cd ~/spectre-mine/ && ./start.sh" > ~/.bashrc
 else
-  if ! grep -Fxq "cd ~/spectre-mine/ && sh start.sh" ~/.bashrc; then
-    echo "cd ~/spectre-mine/ && sh start.sh" >> ~/.bashrc
+  if ! grep -Fxq "cd ~/spectre-mine/ && ./start.sh" ~/.bashrc; then
+    echo "cd ~/spectre-mine/ && ./start.sh" >> ~/.bashrc
   fi
 fi
