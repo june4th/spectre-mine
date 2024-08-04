8e8b0312258051ddd50752476a0f000813a24af8
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-08-05 00:10:49 +0700

    Update and rename test2.sh to test3.sh

--- test3.sh
+++ test3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Nhập tên worker mới (hoặc nhấn Enter để giữ nguyên JOY3): " 
-read new_worker_name < /dev/bash
+read new_worker_name < /dev/tty
 [ -z "$new_worker_name" ]&&new_worker_name="JOY3"
 sed -i "s/--worker-name JOY3/--worker-name $new_worker_name/g" start.sh
 echo "Tên worker đã được cập nhật thành $new_worker_name"
