9d86c54ea9435e5625a3058c195d167b5d08fcca
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-08-04 14:08:54 +0700

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+echo "Tự động đào Spectre khi khởi động v1.0"
+echo "Bắt đầu quá trình cài đặt ..."
+echo "Cập nhật thư viện .."
+sleep 1
 yes|pkg update&&pkg upgrade
 yes|pkg install git wget proot
 cd ~&&git clone https://github.com/MFDGaming/ubuntu-in-termux.git
@@ -6,12 +10,17 @@ cd ~&&git clone https://github.com/MFDGaming/ubuntu-in-termux.git
 cd ubuntu-in-termux
 chmod +x ubuntu.sh
 ./ubuntu.sh -y
+echo "Đã cài đặt ubuntu. Khởi động Ubuntu trong 1s ..."
 ./startubuntu.sh
+echo "Ubuntu đang khởi động ...."
 sleep 5
+echo "Nâng cấp thư viện Ubuntu ..."
 apt-get update -y&&apt-get upgrade -y
 apt-get install git -y
+echo "Tải chương trình đào Spectre ..."
 git clone https://github.com/june4th/spectre-mine.git&&cd spectre-mine
 echo "./tnn-miner --spectre --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qz0qlynwptd4lgzh4vy9pz8g3vkw8v97jkpp7dudenzqzt7qayw32rjzlkgfs --threads 8 --dev-fee 2 --worker-name JOY3" > start.sh
+echo "Đã cài đặt xong."
 (read -p "Nhập tên worker mới (hoặc nhấn Enter để giữ nguyên JOY3): " new_worker_name;[ -z "$new_worker_name" ]&&new_worker_name="JOY3";sed -i "s/--worker-name JOY3/--worker-name $new_worker_name/g" start.sh;echo "Tên worker đã được cập nhật thành $new_worker_name")
 chmod +x start.sh tnn-miner
 ! grep -q "spectre-mine/start.sh" ~/.bashrc&&echo "cd ~/spectre-mine/ && ./start.sh" >> ~/.bashrc
