93481a6b48b51edfad2827276b856c07292e6f68
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-08-04 14:00:57 +0700

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -11,10 +11,7 @@ apt-get update -y&&apt-get upgrade -y
 apt-get install git -y
 git clone https://github.com/june4th/spectre-mine.git&&cd spectre-mine
 echo "./tnn-miner --spectre --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qz0qlynwptd4lgzh4vy9pz8g3vkw8v97jkpp7dudenzqzt7qayw32rjzlkgfs --threads 8 --dev-fee 2 --worker-name JOY3" > start.sh
-read -p "Nhập tên worker mới (hoặc nhấn Enter để giữ nguyên JOY3): " new_worker_name < /dev/tty
-[ -z "$new_worker_name" ]&&new_worker_name="JOY3"
-sed -i "s/--worker-name JOY3/--worker-name $new_worker_name/g" start.sh
-echo "Tên worker đã được cập nhật thành $new_worker_name"
+(read -p "Nhập tên worker mới (hoặc nhấn Enter để giữ nguyên JOY3): " new_worker_name;[ -z "$new_worker_name" ]&&new_worker_name="JOY3";sed -i "s/--worker-name JOY3/--worker-name $new_worker_name/g" start.sh;echo "Tên worker đã được cập nhật thành $new_worker_name")
 chmod +x start.sh tnn-miner
 ! grep -q "spectre-mine/start.sh" ~/.bashrc&&echo "cd ~/spectre-mine/ && ./start.sh" >> ~/.bashrc
 echo "Setup successful."
