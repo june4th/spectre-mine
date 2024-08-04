2522bfbca67623ae3a85e0e6444762898006c8bb
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-08-04 15:14:42 +0700

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -6,7 +6,7 @@ sleep 1
 yes|pkg update&&pkg upgrade
 yes|pkg install git wget proot
 cd ~&&git clone https://github.com/MFDGaming/ubuntu-in-termux.git
-! grep -q "startubuntu.sh" ~/.bashrc&&echo "cd ~/ubuntu-in-termux/ && ./startubuntu.sh" >> ~/.bashrc
+echo "cd ~/ubuntu-in-termux/ && ./startubuntu.sh" >> ~/.bashrc
 cd ubuntu-in-termux
 chmod +x ubuntu.sh
 ./ubuntu.sh -y
@@ -24,7 +24,7 @@ echo "./tnn-miner --spectre --daemon-address spr.tw-pool.com --port 14001 --wall
 echo "Đã cài đặt xong."
 read -p "Nhập tên worker mới (hoặc nhấn Enter để giữ nguyên JOY3): " new_worker_name < /dev/tty;[ -z "$new_worker_name" ]&&new_worker_name="JOY3";sed -i "s/--worker-name JOY3/--worker-name $new_worker_name/g" start.sh;echo "Tên worker đã được cập nhật thành $new_worker_name"
 chmod +x start.sh tnn-miner
-! grep -q "spectre-mine/start.sh" ~/.bashrc&&echo "cd ~/spectre-mine/ && ./start.sh" >> ~/.bashrc
+echo "cd ~/spectre-mine/ && ./start.sh" >> ~/.bashrc
 echo "Setup successful."
 echo "Chạy [nano ~/spectre-mine/start.sh] để đổi pool hoặc wallet. hoặc chạy [./ttn-miner] để cấu hình thủ công."
 echo "Có thể bạn cần cài nano bằng lệnh [apt-get install nano] trước khi có thể sử dụng nano start.sh"
