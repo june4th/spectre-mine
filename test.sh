be9959d138c15edd1376cf26f1ebb806a44e7ff8
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-08-04 23:59:09 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ sleep 5
 echo "Đã truy cập vào Ubuntu."
 cd spectre-mine
 echo "Nhập tên worker mới (hoặc nhấn Enter để giữ nguyên JOY3): " 
-new_worker_name=$(termux-dialog text -i "" -t "Nhập tên nhân viên mới:")
+read -p "" new_worker_name # Sử dụng read -p
 [ -z "$new_worker_name" ]&&new_worker_name="JOY3"
 sed -i "s/--worker-name JOY3/--worker-name $new_worker_name/g" start.sh
 echo "Tên worker đã được cập nhật thành $new_worker_name"
