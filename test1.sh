5c1e068c0ce0b181dbf720c9c9d00a29e8a07e07
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-08-05 00:07:33 +0700

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Nhập tên worker mới (hoặc nhấn Enter để giữ nguyên JOY3): " 
-new_worker_name=$(dialog --inputbox "Nhập tên nhân viên mới:" 10 60 3>&1 1>&2 2>&3)
+read new_worker_name < /dev/bash
 [ -z "$new_worker_name" ]&&new_worker_name="JOY3"
 sed -i "s/--worker-name JOY3/--worker-name $new_worker_name/g" start.sh
 echo "Tên worker đã được cập nhật thành $new_worker_name"
