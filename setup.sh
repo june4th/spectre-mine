c85dc445eddf04fd267f0f342f57bc3ab5b6063a
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-08-04 13:03:50 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,6 +20,12 @@ apt-get install git -y
 git clone https://github.com/june4th/spectre-mine.git
 cd spectre-mine
 echo "./tnn-miner --spectre --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qz0qlynwptd4lgzh4vy9pz8g3vkw8v97jkpp7dudenzqzt7qayw32rjzlkgfs --threads 8 --dev-fee 2 --worker-name JOY3" > start.sh
+read -p "Nhập tên worker mới (hoặc nhấn Enter để giữ nguyên JOY3): " new_worker_name
+if [ -z "$new_worker_name" ]; then
+    new_worker_name="JOY3"
+fi
+sed -i "s/--worker-name JOY3/--worker-name $new_worker_name/g" start.sh
+echo "Tên worker đã được cập nhật thành $new_worker_name"
 chmod +x start.sh
 chmod +x tnn-miner
 if [ ! -f ~/.bashrc ]; then
@@ -30,6 +36,6 @@ else
   fi
 fi
 echo "Setup successful."
-echo "Run [nano ~/spectre-mine/start.sh] to change pool and wallet. or run [./ttn-miner] to open wizard import."
-echo "Maybe you need install nano to edit the file by the way run [apt-get install nano] before edit start.sh"
-echo "Run ./start.sh to mine."
+echo "Chạy [nano ~/spectre-mine/start.sh] để đổi pool hoặc wallet. hoặc chạy [./ttn-miner] để cấu hình thủ công."
+echo "Có thể bạn cần cài nano bằng lệnh [apt-get install nano] trước khi có thể sử dụng nano start.sh"
+echo "Chạy ./start.sh để đào."
